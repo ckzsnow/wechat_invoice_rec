dfda26a591d9d48536fffe75d23a4367b4b5c7a2
burninglee <15921961656@163.com>
2018-07-23 10:39:30 +0800

    modify

--- src/main/java/com/bbz/controller/web/WebController.java
+++ src/main/java/com/bbz/controller/web/WebController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
